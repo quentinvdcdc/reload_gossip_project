43a0acd29cf9d31ba0a3de0430dd1017b64b043a
quentinvdcdc <quentin.vandecandelaere@gmail.com>
2019-09-28 17:40:27 +0200

    [update] adding resource for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  
 	root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/team', to: 'static_pages#team'
   get '/bienvenue/:first_name', to: 'static_pages#bienvenue', as: 'bienvenue'
-  get '/gossip/:id', to: 'gossip#show', as: 'gossip_show'
-  get 'user/:id', to: 'user#show', as: 'user_show'
+  resources :users
+  resources :gossips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
